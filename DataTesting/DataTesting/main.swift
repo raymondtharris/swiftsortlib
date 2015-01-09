8960c2edeed7ab8246678e316a6a382f5efd901b
Tim Harris <raymondtimothyharris@gmail.com>
2015-01-09 13:26:01 -0800

    New project added to test sorting library. Added smalldataset variable to use for sorting.

---
+++ DataTesting/DataTesting/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  DataTesting
+//
+//  Created by Tim Harris on 1/9/15.
+//  Copyright (c) 2015 Tim Harris. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+
+var smallDataSet = [4,3,5,7,9,23,456,234,32,74,45,134,743,7956,3456,4211,323,412,55454,37,654,342,36,12412]
